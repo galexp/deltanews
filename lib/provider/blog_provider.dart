dda750d0ade3e5fda64430878cb7225395170c70
galexp <40064291+galexp@users.noreply.github.com>
2022-12-09 15:34:43 +0100

    fetch blog from server

--- lib/provider/blog_provider.dart
+++ lib/provider/blog_provider.dart
@@ -9,8 +9,50 @@ import 'package:http/http.dart';
 class BlogProvider with ChangeNotifier{
   Status _isLoadingData = Status.notLoading;
 
+
   Status get isLoadingData => _isLoadingData;
 
+// fetch all data of blog from the server
+  Future<List<Blog>> getBlogs() async{
+
+
+     final response = await get(Uri.parse(HttpService.blog),
+      headers: {
+        'content-Type': 'application/json',
+       },
+      ).then(onValueList);
+
+
+    return response;
+  }
+
+// this is to get all response from the server
+   Future<List<Blog>> onValueList(Response response) async{
+
+    List<Blog> result = [];
+
+    final Map<String, dynamic> responseData = json.decode(response.body);
+  
+    if(response.statusCode == 200){
+
+      for (var element in responseData['data']) {
+         Blog blog = Blog.fromJson(element);
+         result.add(blog);
+      }
+      
+     
+
+    
+    }
+
+    
+
+    return result;
+
+  }
+
+ 
+  // this is to get save 
   Future<Map<String, dynamic>> saveBlog(String title, String body, 
   String author, String token) async{
 
@@ -50,7 +92,7 @@ class BlogProvider with ChangeNotifier{
       print(responseData);
       
       Blog blog = Blog.fromJson(responseData['data']);
- print(blog.body);
+
       result = {
         'status' : 200,
         'message' : responseData['message'],
@@ -75,4 +117,6 @@ class BlogProvider with ChangeNotifier{
     print(error);
     return {'status': false, 'message': 'Unsuccessful Request', 'data': error};
   }
+
+ 
 }
--- lib/widgets/homescreen.dart
+++ lib/widgets/homescreen.dart
@@ -1,7 +1,9 @@
+import 'package:deltanews/provider/blog_provider.dart';
 import 'package:deltanews/screens/single_news.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreenWidget extends StatefulWidget {
   const HomeScreenWidget({super.key});
@@ -15,6 +17,10 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height / 4;
     var width = MediaQuery.of(context).size.width / 4;
+
+    // call the blog provider to display the list of blogs
+    // final blogProvider = Provider.of<BlogProvider>(context);
+
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -69,44 +75,57 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
               ],
             ),
           ),
-          CarouselSlider(
-            options: CarouselOptions(
-                enableInfiniteScroll: true,
-                aspectRatio: 20 / 9,
-                viewportFraction: 0.9,
-                autoPlay: true,
-                enlargeCenterPage: true,
-                pauseAutoPlayOnTouch: true,
-                enlargeStrategy: CenterPageEnlargeStrategy.scale),
-            items: [4].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                    margin: const EdgeInsets.only(top: 5),
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 20, bottom: 30),
-                    width: double.infinity,
-                    height: 200,
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/obi.png'),
-                            fit: BoxFit.fill)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: const [
-                        Text(
-                          '2023: Peter Obi Promises a new Nigeria devoid of poverty',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Lato",
-                              fontSize: 15),
-                        )
-                      ],
-                    ),
+          FutureBuilder(
+            future: BlogProvider().getBlogs(),
+            builder: (context, snapshot) {
+              if(snapshot.hasData){
+                  return CarouselSlider(
+                options: CarouselOptions(
+                    enableInfiniteScroll: true,
+                    aspectRatio: 20 / 9,
+                    viewportFraction: 0.9,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    pauseAutoPlayOnTouch: true,
+                    enlargeStrategy: CenterPageEnlargeStrategy.scale),
+                items: [4].map((i) {
+                  print(i);
+                  return Builder(
+                    builder: ( context) {
+                      return Container(
+                        margin: const EdgeInsets.only(top: 5),
+                        padding:
+                            const EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                        width: double.infinity,
+                        height: 200,
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/obi.png'),
+                                fit: BoxFit.fill)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children:  [
+                            Text(
+                              snapshot.data![i].title,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "Lato",
+                                  fontSize: 15),
+                            )
+                          ],
+                        ),
+                      );
+                    },
                   );
-                },
+                }).toList(),
               );
-            }).toList(),
+          
+              }else{
+                return const Center(
+                    child:  CircularProgressIndicator(),
+                  );
+              }
+              }
           ),
           Container(
             padding: const EdgeInsets.only(left: 20, right: 20),
@@ -153,127 +172,142 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
             width: double.infinity,
             height: height * 2,
             padding: const EdgeInsets.all(20),
-            child: ListView.builder(
-              itemCount: 14,
-              itemBuilder: (BuildContext context, int index) {
-                var boxwidth = MediaQuery.of(context).size.width / 4;
-                return Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  width: double.infinity,
-                  height: 180,
-                  decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      border: Border.all(color: Colors.blue)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: double.infinity,
-                        width: boxwidth * 1.5,
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image:
-                                    AssetImage('assets/images/newsimage.png'),
-                                fit: BoxFit.fill)),
-                      ),
-                      Container(
-                        width: boxwidth * 2,
-                        height: double.infinity,
-                        clipBehavior: Clip.none,
-                        child: Column(
-                          children: [
-                            Container(
-                              width: double.infinity,
-                              height: 60,
-                              padding: const EdgeInsets.all(5),
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const SIngleNewsScreen()));
-                                },
-                                child: const Text(
-                                  'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum ',
-                                  softWrap: true,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.fade,
-                                  style: TextStyle(
-                                      fontFamily: "Lato", fontSize: 15),
+            child: FutureBuilder(
+              future: BlogProvider().getBlogs(),
+              builder: (context, snapshot) {
+                if(snapshot.hasData){
+
+                   return ListView.builder(
+                  reverse: true,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, int index) {
+                    var boxwidth = MediaQuery.of(context).size.width / 4;
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      width: double.infinity,
+                      height: 180,
+                      decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(Radius.circular(20)),
+                          border: Border.all(color: Colors.blue)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: double.infinity,
+                            width: boxwidth * 1.5,
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('assets/images/newsimage.png'),
+                                    fit: BoxFit.fill)),
+                          ),
+                          Container(
+                            width: boxwidth * 2,
+                            height: double.infinity,
+                            clipBehavior: Clip.none,
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: double.infinity,
+                                  height: 60,
+                                  padding: const EdgeInsets.all(5),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const SIngleNewsScreen()));
+                                    },
+                                    child:  Text(
+                                      "${snapshot.data![index].title}\n${snapshot.data![index].body}",
+                                      softWrap: true,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.fade,
+                                      style: const TextStyle(
+                                          fontFamily: "Lato", fontSize: 15),
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ),
-                            ListTile(
-                              leading: const Image(
-                                image: AssetImage('assets/images/logo.png'),
-                                width: 25,
-                              ),
-                              title: const Text(
-                                "Delta News",
-                                style:
-                                    TextStyle(fontFamily: "Lato", fontSize: 12),
-                              ),
-                              horizontalTitleGap: .1,
-                              trailing: TextButton(
-                                  style: TextButton.styleFrom(
-                                      minimumSize: const Size(8, 8),
-                                      backgroundColor: Colors.white,
-                                      shape: const RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(3)),
-                                        side: BorderSide(
-                                            color: Colors.blue, width: 1),
+                                ListTile(
+                                  leading: const Image(
+                                    image: AssetImage('assets/images/logo.png'),
+                                    width: 25,
+                                  ),
+                                  title: const Text(
+                                    "Delta News",
+                                    style:
+                                        TextStyle(fontFamily: "Lato", fontSize: 12),
+                                  ),
+                                  horizontalTitleGap: .1,
+                                  trailing: TextButton(
+                                      style: TextButton.styleFrom(
+                                          minimumSize: const Size(8, 8),
+                                          backgroundColor: Colors.white,
+                                          shape: const RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(3)),
+                                            side: BorderSide(
+                                                color: Colors.blue, width: 1),
+                                          )),
+                                      onPressed: () {},
+                                      child: const Text(
+                                        "News",
+                                        style: TextStyle(fontSize: 10),
                                       )),
-                                  onPressed: () {},
-                                  child: const Text(
-                                    "News",
-                                    style: TextStyle(fontSize: 10),
-                                  )),
-                            ),
-                            Container(
-                              height: 50,
-                              width: double.infinity,
-                              clipBehavior: Clip.none,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: double.infinity,
+                                  clipBehavior: Clip.none,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
+                                      Row(
+                                        children: [
+                                          IconButton(
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                PhosphorIcons.thumbs_up_light,
+                                                color: Colors.blue,
+                                              )),
+                                          const Text('32k'),
+                                          IconButton(
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                PhosphorIcons
+                                                    .chat_centered_dots_light,
+                                                color: Colors.blue,
+                                              )),
+                                          const Text('11.5k'),
+                                        ],
+                                      ),
                                       IconButton(
                                           onPressed: () {},
                                           icon: const Icon(
-                                            PhosphorIcons.thumbs_up_light,
+                                            PhosphorIcons.bookmark_light,
                                             color: Colors.blue,
-                                          )),
-                                      const Text('32k'),
-                                      IconButton(
-                                          onPressed: () {},
-                                          icon: const Icon(
-                                            PhosphorIcons
-                                                .chat_centered_dots_light,
-                                            color: Colors.blue,
-                                          )),
-                                      const Text('11.5k'),
+                                          ))
                                     ],
                                   ),
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(
-                                        PhosphorIcons.bookmark_light,
-                                        color: Colors.blue,
-                                      ))
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
                 );
-              },
+
+                }else{
+                  return const Center(
+                    child:  CircularProgressIndicator(),
+                  );
+                }
+               
+              }
             ),
           )
         ],
